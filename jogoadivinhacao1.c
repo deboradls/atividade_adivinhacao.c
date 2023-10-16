984fadc890ad7e99613034085ea044270b38fbb2
Débora <deb.limad@gmail.com>
2023-10-15 23:39:01 -0300

    Escolha dos limites do número

---
+++ jogoadivinhacao1.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NUMERO_DE_TENTATIVAS 3
+
+int main() {
+    printf("\n\n");
+    printf("          P  /_\\  P                              \n");
+    printf("         /_\\_|_|_/_\\                            \n");
+    printf("     n_n | ||. .|| | n_n         Bem vindo ao     \n");
+    printf("     |_|_|nnnn nnnn|_|_|     Jogo de Adivinhação! \n");
+    printf("    |\" \"  |  |_|  |\"  \" |                     \n");
+    printf("    |_____| ' _ ' |_____|                         \n");
+    printf("          \\__|_|__/                              \n");
+    printf("\n\n");
+
+    int chute;
+    int acertou;
+    int nivel;
+    int totaldetentativas;
+    double pontos = 1000;
+
+    // variaveis para armazenar os limites escolhidos
+    int limite_inferior, limite_superior; 
+
+    printf("Escolha o limite inferior para o número secreto: ");
+    scanf("%d", &limite_inferior);
+    printf("Escolha o limite superior para o número secreto: ");
+    scanf("%d", &limite_superior);
+
+    srand(time(0));
+
+    // use os limites escolhidos pelo usuário para gerar o número secreto
+    int numerosecreto = rand() % (limite_superior - limite_inferior + 1) + limite_inferior;
+
+    printf("Qual o nível de dificuldade?\n");
+    printf("(1) Fácil (2) Médio (3) Difícil\n\n");
+    printf("Escolha: ");
+    scanf("%d", &nivel);
+
+    switch (nivel) {
+        case 1:
+            totaldetentativas = 20;
+            break;
+        case 2:
+            totaldetentativas = 15;
+            break;
+        default:
+            totaldetentativas = 6;
+            break;
+    }
+
+    for (int i = 1; i <= totaldetentativas; i++) {
+        printf("-> Tentativa %d de %d\n", i, totaldetentativas);
+        printf("Chute um número: ");
+        scanf("%d", &chute);
+
+        if (chute < limite_inferior || chute > limite_superior) {
+            printf("Seu chute está fora dos limites escolhidos. Tente novamente.\n");
+            i--;
+            continue;
+        }
+
+        acertou = chute == numerosecreto;
+
+        if (acertou) {
+            break;
+        } else if (chute > numerosecreto) {
+            printf("\nSeu chute foi maior do que o número secreto!\n\n");
+        } else {
+            printf("\nSeu chute foi menor do que o número secreto!\n\n");
+        }
+
+        double pontosperdidos = abs(chute - numerosecreto) / 2.0;
+        pontos = pontos - pontosperdidos;
+    }
+
+    printf("\n");
+    if (acertou) {
+		printf("             OOOOOOOOOOO               \n");
+		printf("         OOOOOOOOOOOOOOOOOOO           \n");
+		printf("      OOOOOO  OOOOOOOOO  OOOOOO        \n");
+		printf("    OOOOOO      OOOOO      OOOOOO      \n");
+		printf("  OOOOOOOO  #   OOOOO  #   OOOOOOOO    \n");
+		printf(" OOOOOOOOOO    OOOOOOO    OOOOOOOOOO   \n");
+		printf("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  \n");
+		printf("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  \n");
+		printf("OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO  \n");
+		printf(" OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO   \n");
+		printf("  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO    \n");
+		printf("    OOOOO   OOOOOOOOOOOOOOO   OOOO     \n");
+		printf("      OOOOOO   OOOOOOOOO   OOOOOO      \n");
+		printf("         OOOOOO         OOOOOO         \n");
+		printf("             OOOOOOOOOOOO              \n");
+        printf("Parabéns! Você acertou!\n");
+        printf("Você fez %.2f pontos. Até a próxima!\n\n", pontos);
+    } else {
+		printf("       \\|/ ____ \\|/    \n");   
+        printf("        @~/ ,. \\~@      \n");   
+        printf("       /_( \\__/ )_\\    \n");   
+        printf("          \\__U_/        \n");
+        printf("Você perdeu! Tente novamente!\n\n");
+    }
+
+    return 0;
+}
